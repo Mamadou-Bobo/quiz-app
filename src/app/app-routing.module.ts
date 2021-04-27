2d8154a0cf6be93f9767f68ef30628c72c350af6
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2021-04-27 19:53:29 +0000

    ajout de la route questions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LevelComponent } from './level/level.component';
+import { QuestionsComponent } from './questions/questions.component';
 import { QuizComponent } from './quiz/quiz.component';
 
 const routes: Routes = [
   {path: '', component: QuizComponent},
-  {path: 'level', component: LevelComponent}
+  {path: 'level', component: LevelComponent},
+  {path: 'questions', component: QuestionsComponent}
 ];
 
 @NgModule({
