aa9efa1b546701b58003efb8f5b1578e8b8e5d25
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2021-04-27 21:19:30 +0000

    ajout de la variable isClicked

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
+  isClicked: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.isClicked = true;
   }
 
 }
