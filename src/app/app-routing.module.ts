f6c6e4f9c92524901a39a82b33ba91105bdaa71c
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2021-04-27 15:20:36 +0000

    ajout des routes pour la navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LevelComponent } from './level/level.component';
+import { QuizComponent } from './quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: QuizComponent},
+  {path: 'level', component: LevelComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
