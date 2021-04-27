f86b82d41f47ec5a9e18e54ab042f2761345bc4d
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2021-04-27 03:16:50 +0000

    desactivation du bouton lorqu'une catégorie n'est pas sélectionnée

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { QuizService } from '../services/quiz.service';
 
 @Component({
   selector: 'app-button',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  isClicked: boolean = false;
+  clickSubscription: Subscription | undefined;
+
+  constructor(private quizService: QuizService) { }
 
   ngOnInit(): void {
+    this.clickSubscription = this.quizService.isClickedSubject.subscribe(
+      (value) => {
+        this.isClicked = value;
+      }
+    )
   }
+
+
 
 }
