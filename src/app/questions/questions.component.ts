4a76f40e4e96ae389d65e88f0651fe5c482e3b69
Mamadou-Bobo <diallomamadoubobo999@gmail.com>
2021-04-28 00:25:04 +0000

    récupération des données de l'url et rédirection si les tableau est vide

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { QuizService } from '../services/quiz.service';
 
 @Component({
   selector: 'app-questions',
@@ -9,10 +11,24 @@ export class QuestionsComponent implements OnInit {
 
   isClicked: boolean = false;
 
-  constructor() { }
+  constructor(private quizService: QuizService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.isClicked = true;
+    
+    console.log(this.quizService.category);
+    console.log(this.quizService.difficulty);
+
+    this.quizService.getQuestion(this.quizService.category, this.quizService.difficulty)
+    .subscribe((data: any) =>  {
+      console.log(data.results);
+      if(data.results.length === 0) {
+        // console.log("vide");
+        this.router.navigate(['/']);
+      }
+    });
+
   }
 
 }
